81da9e91407eb3ae60575bc1f3f88a54ca53bd1a
Kirimino <brianonduso08@gmail.com>
2024-07-07 18:50:21 +0300

    complete lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
+        response = requests.get(self.url)
+        return response.content
         pass
 
     def load_json(self):
+        response_body = self.get_response_body()
+        try:
+            json_data = json.loads(response_body)
+            return json_data
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON: {str(e)}")
+            return None
         pass
